2815c4d9168cd7e4eb7a768deec8005ffcf56710
Elliot Hershberg <eahershberg@gmail.com>
2021-01-21 09:54:19 -0800

    Create rough sketch with hardcoded data

--- src/components/genome_browser/index.js
+++ src/components/genome_browser/index.js
@@ -1,112 +1,165 @@
 import React from "react";
+import { connect } from "react-redux";
+// import Card from "../framework/card";
 import {
   createViewState,
   createJBrowseTheme,
   JBrowseLinearGenomeView,
-  ThemeProvider
+  ThemeProvider,
 } from "@jbrowse/react-linear-genome-view";
 
 const theme = createJBrowseTheme();
 
+// Data Views
+
+/* 
+Load the assembly from metadata
+*/
+
+/* 
+Load the gene track from JSON (from entropyCreateStateFromJsons)
+
+We want to load the gene array into a FeatureTrack (can FeatureTrack display be configured?)
+*/
+
+/* 
+Wiggle track of entropy
+
+1. We want the same entropy data as entropy panel (probably inline from entropy)
+2. Process the entropy array
+3. Put it into wiggle track (Need to make the wiggle track run callback on click (?))
+*/
+
+// Future JB2 Work
+
+/* 
+1. Floating track labels
+2. Ability to upload tracks into embedded LGV
+3. Theming
+4. Weird width (CSS?) interactions
+*/
+
+// Question is whether we want to replace diversity panel or just add browser
+
+
+// get from parent
+import * as data from "./data/ncov_global_root-sequence.json";
+
+// This is the hard-coded Sars-Cov2 assembly
+
+// const assembly = {
+//   name: "Sars-Cov2",
+//   sequence: {
+//     type: "ReferenceSequenceTrack",
+//     trackId: "Sars-Cov2-ReferenceSequenceTrack",
+//     adapter: {
+//       type: "BgzipFastaAdapter",
+//       fastaLocation: {
+//         uri: "https://jbrowse.org/genomes/sars-cov2/fasta/sars-cov2.fa.gz",
+//       },
+//       faiLocation: {
+//         uri: "https://jbrowse.org/genomes/sars-cov2/fasta/sars-cov2.fa.gz.fai",
+//       },
+//       gziLocation: {
+//         uri: "https://jbrowse.org/genomes/sars-cov2/fasta/sars-cov2.fa.gz.gzi",
+//       },
+//     },
+//   },
+// };
+
 const assembly = {
-  name: 'GRCh38',
+  name: "Sars-Cov2",
   sequence: {
-    type: 'ReferenceSequenceTrack',
-    trackId: 'GRCh38-ReferenceSequenceTrack',
+    type: "ReferenceSequenceTrack",
+    trackId: "Sars-Cov2-ReferenceSequenceTrack",
     adapter: {
-      type: 'BgzipFastaAdapter',
-      fastaLocation: {
-        uri:
-          'http://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna_index/Homo_sapiens.GRCh38.dna.toplevel.fa.gz'
-      },
-      faiLocation: {
-        uri:
-          'http://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna_index/Homo_sapiens.GRCh38.dna.toplevel.fa.gz.fai'
-      },
-      gziLocation: {
-        uri:
-          'http://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/dna_index/Homo_sapiens.GRCh38.dna.toplevel.fa.gz.gzi'
-      }
-    }
+      type: "FromConfigSequenceAdapter",
+      features: [
+        {
+          refName: "Sars-Cov2",
+          uniqueId: "Sars-Cov2",
+          start: 0,
+          end: data.nuc.length,
+          seq: data.nuc,
+        },
+      ],
+    },
   },
-  aliases: ['hg38'],
-  refNameAliases: {
-    adapter: {
-      type: 'RefNameAliasAdapter',
-      location: {
-        uri:
-          'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt'
-      }
-    }
-  }
 };
 
 const tracks = [
   {
-    type: 'FeatureTrack',
-    trackId: 'ncbi_refseq_109_hg38',
-    name: 'NCBI RefSeq (GFF3Tabix)',
-    assemblyNames: ['GRCh38'],
-    category: ['Annotation'],
+    type: "FeatureTrack",
+    name: "Sars-Cov2 Annotations",
+    trackId: "sars-cov2-annotations",
+    assemblyNames: ["Sars-Cov2"],
+    category: ["Annotation"],
     adapter: {
-      type: 'Gff3TabixAdapter',
+      type: "Gff3TabixAdapter",
       gffGzLocation: {
-        uri:
-          'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz'
+        uri: "https://jbrowse.org/genomes/sars-cov2/sars-cov2-annotations.sorted.gff.gz",
       },
       index: {
         location: {
-          uri:
-            'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.tbi'
-        }
-      }
-    }
-  }
+          uri: "https://jbrowse.org/genomes/sars-cov2/sars-cov2-annotations.sorted.gff.gz.tbi",
+        },
+      },
+    },
+  },
 ];
 
 const defaultSession = {
-  name: 'My session',
+  name: "My session",
   view: {
-    id: 'linearGenomeView',
-    type: 'LinearGenomeView',
+    id: "linearGenomeView",
+    type: "LinearGenomeView",
     tracks: [
       {
-        type: 'ReferenceSequenceTrack',
-        configuration: 'GRCh38-ReferenceSequenceTrack',
+        type: "ReferenceSequenceTrack",
+        configuration: "Sars-Cov2-ReferenceSequenceTrack",
         displays: [
           {
-            type: 'LinearReferenceSequenceDisplay',
-            configuration:
-              'GRCh38-ReferenceSequenceTrack-LinearReferenceSequenceDisplay'
-          }
-        ]
+            type: "LinearReferenceSequenceDisplay",
+            configuration: "Sars-Cov2-ReferenceSequenceTrack-LinearReferenceSequenceDisplay",
+          },
+        ],
       },
-      {
-        type: 'FeatureTrack',
-        configuration: 'ncbi_refseq_109_hg38',
-        displays: [
-          {
-            type: 'LinearBasicDisplay',
-            configuration: 'ncbi_refseq_109_hg38-LinearBasicDisplay'
-          }
-        ]
-      }
-    ]
-  }
+    ],
+  },
 };
 
-function View() {
-  const state = createViewState({
-    assembly,
-    tracks,
-    location: '10:29,838,737..29,838,819',
-    defaultSession
-  });
-  return (
-    <ThemeProvider theme={theme}>
-      <JBrowseLinearGenomeView viewState={state} />
-    </ThemeProvider>
-  );
+class GenomeView extends React.Component {
+  render() {
+    console.log(this.props);
+
+    // add assembly from metadata
+
+    // add tracks from annotations
+
+    // move default session here and set it up
+
+    const viewState = createViewState({
+      assembly,
+      tracks,
+      location: "1:1000",
+      defaultSession,
+    });
+
+    return (
+      // Ultimately I will want to wrap this in a Card for consistency
+      <ThemeProvider theme={theme}>
+        <JBrowseLinearGenomeView viewState={viewState} />
+      </ThemeProvider>
+    );
+  }
 }
 
-export default View;
+function mapStateToProps(state) {
+  return {
+    annotations: state.entropy.annotations,
+    geneMap: state.entropy.geneMap,
+    geneLength: state.controls.geneLength,
+    metadata: state.metadata,
+  };
+}
+export default connect(mapStateToProps)(GenomeView);
